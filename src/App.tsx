4fe0ff241433e912f2e6032d112ba635650ad123
Murat <murat_a@mail.ru>
2026-05-01 19:13:57 +0500

    feat: initialize App component with HashRouter and basic page routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navigation/Navbar';
 import MapPage from './pages/MapPage';
 import DashboardPage from './pages/DashboardPage';
 
 function App() {
     return (
-        <BrowserRouter>
+        <HashRouter>
             <div className="w-screen h-screen overflow-hidden bg-slate-50 flex flex-col font-sans text-slate-800">
                 <Navbar />
                 <div className="flex-1 w-full relative overflow-hidden flex">
@@ -15,7 +15,7 @@ function App() {
                     </Routes>
                 </div>
             </div>
-        </BrowserRouter>
+        </HashRouter>
     );
 }
 
